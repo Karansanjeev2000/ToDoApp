3607e4c06107d791dfbe4424716cf255538f9d64
karansanjeev2000@gmail.com <karansanjeev2000@gmail.com>
2023-09-09 15:55:27 +0530

    "done"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserServiceService } from './services/user-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoApp';
+  constructor(private ser:UserServiceService){}
+  
+
+  logoutf(){
+    localStorage.removeItem('token');
+    this.ser.isLoggedIn=false;
+    this.ser.navigateToLoginView();
+  }
 }
